0c9c75526a9be1ac912fd3e8af46b27ff0feb612
Brand Danish <danishiqbaldi74@gmail.com>
2025-06-06 11:57:28 -0700

    preparing for deployment

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,7 @@
-const app=require('./src/app');
+const app=require('../src/app');
 require('dotenv').config();
 const port=3000;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
+module.exports=app;
